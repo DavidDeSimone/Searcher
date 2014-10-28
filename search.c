f1b2b7828d783ccca1db3a46ba96a1168865518c
David DeSimone <djd231@scarletmail.rutgers.edu>
2014-10-27 23:57:08 -0400

    Added main skeleton

--- search.c
+++ search.c
@@ -1,1 +1,30 @@
-search.c
+#include "search.h"
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+
+int main(int argc. char **args) {
+  ListIndexObjList list;
+  char input[50];
+  char **to_find;
+
+
+  if(argc != 2) {
+    printf("Invalid number of command line arguments!\n");
+    return -1;
+  }
+
+  list = list_init(args[1]);
+
+  while(TRUE) {
+    scanf("%s", &input);
+    
+
+
+  }
+
+
+  list_dec(list);
+  return 0;
+}
--- search.h
+++ search.h
@@ -1,1 +1,25 @@
-search.h
+#include <stdio.h>
+#include "Index/index.h"
+
+/* Constructor for Object that maintains word frequencies indexed by document
+ * See Index/index.h for more details
+ */
+void list_init(char *file_name);
+
+/* Destructor for LinkedIndexObjList
+ * Call when list should be freed
+ */
+void list_dec(LinkedIndexObjList *list);
+
+
+/* Search for files containing the given term. A query may contain
+ * one or more terms. The search term will output the names of the files 
+ * that contain ALL of the terms in the query
+ */
+char** sa(char **to_find);
+
+/* Searches for files containing the given term. Similar to sa
+ * but looks for any documents that contain any subset of terms in the
+ * query
+ */
+char** so(char **to_find);
